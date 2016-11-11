452031695f522825c3d3564c5772883fee158d6f
faifa <faifa@faifadeimac.local>
2016-11-11 16:41:50 +0800

    Initial Commit

---
+++ chart/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  chart
+//
+//  Created by faifa on 16/11/11.
+//  Copyright © 2016年 faifa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ chart/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  chart
+//
+//  Created by faifa on 16/11/11.
+//  Copyright © 2016年 faifa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
